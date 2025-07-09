5e58e11b3864da3b54520092a8a5b644e71215ea
enzo-barbosa <pasquatty@gmail.com>
2025-07-09 20:03:01 -0300

    Adding the @EntityScan annotation to the project's main class to make Spring identify our entities for Spring JPA

--- src/main/java/com/project/todolist/TodolistApplication.java
+++ src/main/java/com/project/todolist/TodolistApplication.java
@@ -2,7 +2,9 @@ package com.project.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan (basePackages = {"com.project.domains", "com.curso.domains.enums"})
 @SpringBootApplication
 public class TodolistApplication {
 
