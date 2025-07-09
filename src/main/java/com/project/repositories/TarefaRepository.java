bee0e72c38745be2cbafc9796b2c4a212a9e26fc
enzo-barbosa <pasquatty@gmail.com>
2025-07-09 19:40:34 -0300

    Pulling dependencies into the main entities interface

--- src/main/java/com/project/repositories/TarefaRepository.java
+++ src/main/java/com/project/repositories/TarefaRepository.java
@@ -1,4 +1,11 @@
 package com.project.repositories;
 
-public interface TarefaRepository {
+import com.project.domains.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    
 }
--- src/main/java/com/project/repositories/UsuarioRepository.java
+++ src/main/java/com/project/repositories/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package com.project.repositories;
 
-public interface UsuarioRepository {
+import com.project.domains.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
 }
