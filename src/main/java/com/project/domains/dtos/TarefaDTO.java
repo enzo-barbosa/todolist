f877c1898b802b90138b1c4af0199ad5149c5cad
enzo-barbosa <pasquatty@gmail.com>
2025-07-17 19:48:48 -0300

    Configuring the UsuarioDTO class

---
+++ src/main/java/com/project/domains/dtos/TarefaDTO.java
@@ -0,0 +1,14 @@
+package com.project.domains.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.domains.Usuario;
+import com.project.domains.enums.StatusTarefa;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public class TarefaDTO {
+
+
+}
---
+++ src/main/java/com/project/domains/dtos/UsuarioDTO.java
@@ -0,0 +1,67 @@
+package com.project.domains.dtos;
+
+import com.project.domains.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UsuarioDTO {
+
+    private Long id;
+
+    @NotNull(message = "O nome não pode ser nulo")
+    @NotBlank(message = "O nome não pode ser vazio")
+    private String nome;
+
+    @NotNull(message = "O email não pode ser nulo")
+    @NotBlank(message = "O email não pode ser vazio")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotNull(message = "A senha não pode ser nulo")
+    @NotBlank(message = "A senha não pode ser vazio")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.email = usuario.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
