14dad23eac9f9d3061b09b324d34ef42386116ce
enzo-barbosa <pasquatty@gmail.com>
2025-07-24 20:22:44 -0300

    Implementando o endpoint findById() na camada Resource - TarefaResource e UsuarioResource

--- src/main/java/com/project/resources/TarefaResource.java
+++ src/main/java/com/project/resources/TarefaResource.java
@@ -1,10 +1,12 @@
 package com.project.resources;
 
+import com.project.domains.Tarefa;
 import com.project.domains.dtos.TarefaDTO;
 import com.project.services.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class TarefaResource {
     @GetMapping //exemplo http://localhost:8080/tarefa
     public ResponseEntity<List<TarefaDTO>> findAll() {
         return ResponseEntity.ok().body(tarefaService.findAll());
+    }
+
+    @GetMapping(value = "/{id}") //exemplo http://localhost:8080/tarefa/1
+    public ResponseEntity<TarefaDTO> findById(@PathVariable Long id) {
+        Tarefa obj = this.tarefaService.findById(id);
+        return ResponseEntity.ok().body(new TarefaDTO(obj));
     }
 }
--- src/main/java/com/project/resources/UsuarioResource.java
+++ src/main/java/com/project/resources/UsuarioResource.java
@@ -1,10 +1,14 @@
 package com.project.resources;
 
+import com.project.domains.Tarefa;
+import com.project.domains.Usuario;
+import com.project.domains.dtos.TarefaDTO;
 import com.project.domains.dtos.UsuarioDTO;
 import com.project.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,11 @@ public class UsuarioResource {
     @GetMapping // exemplo http://localhost:8080/tarefa
     public ResponseEntity<List<UsuarioDTO>> findAll() {
         return ResponseEntity.ok().body(usuarioService.findAll());
+    }
+
+    @GetMapping(value = "/{id}") //exemplo http://localhost:8080/usuario/1
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+        Usuario obj = this.usuarioService.findById(id);
+        return ResponseEntity.ok().body(new UsuarioDTO(obj));
     }
 }
